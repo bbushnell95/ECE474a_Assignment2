ef20ff11be5da1cf8bb3e0a58f450983c33a9a33
bbushnell95 <bbushnell@email.arizona.edu>
2016-10-03 19:34:46 -0700

    Removed ADD and REG .h and .cpp

--- dpgen/src/ADD.cpp
+++
@@ -1,7 +0,0 @@
-/*
-Students: Brett Bushnell (Undergrad), Matt Dzurick (Grad)
-Date Create: 9/29/2016
-Assignment: 2
-File: ADD.cpp
-Description: Cpp file for the ADD struct for method definitions 
-*/
--- dpgen/src/ADD.h
+++
@@ -1,7 +0,0 @@
-/*
-Students: Brett Bushnell (Undergrad), Matt Dzurick (Grad)
-Date Create: 9/29/2016
-Assignment: 2
-File: ADD.h
-Description: .h file for the struct for ADD
-*/
